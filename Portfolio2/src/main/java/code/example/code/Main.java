40e8e3bef26e5ee55dcac22f3baa17420343cd71
AuthorityKind <54275464+AuthorityKind@users.noreply.github.com>
2023-04-02 21:17:59 +0200

    Just needs capacity

--- Portfolio2/src/main/java/code/example/code/Main.java
+++ Portfolio2/src/main/java/code/example/code/Main.java
@@ -3,29 +3,37 @@ package code.example.code;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
 import java.util.ArrayList;
 
-public class Main extends Application{
-
+public class Main extends Application {
     DB db = new DB();
-
     Section departure = new Section("Departure");
     Section arrival = new Section("Arrival");
 
+
     String[] chosenElements = new String[4];
-    Label text;
-    Label voyages = new Label();
+    Label chosenText = new Label("""
+            Chosen Elements:
+            Departure Location:\s
+            Departure Date:\s
+            Arrival Location:\s
+            Arrival Date:\s""");
 
     @Override
     public void start(Stage stage) {
+        int width = 1200;
+        int height = 800;
+
+        //INITIALIZATION
+        VBox voyageGUI = new VBox();
 
         /*
         //Volume
@@ -35,35 +43,33 @@ public class Main extends Application{
         HBox volIndex = new HBox(volText, volField);
          */
 
-        //TextArea
-        text = new Label();
-        text.prefWidth(200);
+        //CHOOSE BUTTON
+        Button chooseButton = new Button("Choose");
+        chooseButton.prefHeight(100);
+        EventHandler<ActionEvent> chooseEvent = actionEvent -> displayChosenElements();
+        chooseButton.setOnAction(chooseEvent);
 
         //SEARCH BUTTON
-        Button searchButton = new Button("Search");
+        Button searchButton = new Button("search");
         searchButton.prefHeight(100);
-        EventHandler<ActionEvent> searchEvent = actionEvent -> {
-            displayChosenElements();
-        };
+        EventHandler<ActionEvent> searchEvent = actionEvent -> displayVoyages(voyageGUI);
         searchButton.setOnAction(searchEvent);
 
-        //COMMIT BUTTON
-        Button commitButton = new Button("Commit");
-        commitButton.prefHeight(100);
-        EventHandler<ActionEvent> commitEvent = actionEvent -> {
-            displayVoyages();
-        };
-        commitButton.setOnAction(commitEvent);
+        //ASSEMBLY
+        VBox upperGUI = new VBox(departure.fullVBox, arrival.fullVBox, chooseButton);
+        upperGUI.setPadding(new Insets(10));
+        VBox lowerGUI = new VBox(chosenText, searchButton);
+        lowerGUI.setPadding(new Insets(30, 10, 10, 10));
+        VBox inputGUI = new VBox(upperGUI, lowerGUI);
 
-        //the rest
-        VBox left = new VBox(departure.fullVBox, arrival.fullVBox, searchButton);
-        VBox right = new VBox(text, commitButton, voyages);
-        BorderPane root = new BorderPane();
-        root.setLeft(left);
-        root.setRight(right);
+        inputGUI.setMinWidth((double) width / 4);
+        voyageGUI.setPadding(new Insets(10));
+        voyageGUI.setMaxWidth(width * 0.75);
 
-        Scene scene = new Scene(root, 500, 500);
+        HBox root = new HBox(inputGUI, voyageGUI);
+        Scene scene = new Scene(root, width, height);
 
+        //STAGE
         stage.setTitle("title");
         stage.setScene(scene);
         stage.show();
@@ -81,7 +87,7 @@ public class Main extends Application{
         addElement(arrival.localComboBox, 2);
         addElement(arrival.dateComboBox, 3);
 
-        text.setText(str);
+        chosenText.setText(str);
     }
 
     private String displayElement(ComboBox<String> box) {
@@ -94,19 +100,24 @@ public class Main extends Application{
 
     private void addElement(ComboBox<String> box, int index) {
         if (box.getValue() != null) {
-            if((index & 1) == 0){
+            if ((index & 1) == 0) {
                 chosenElements[index] = "'" + box.getValue() + "'";
-            } else{
+            } else {
                 chosenElements[index] = db.unparseDate(box.getValue());
             }
         }
     }
 
-    private void displayVoyages() {
+    private void displayVoyages(VBox vbox) {
         ArrayList<String> temp = db.getData(chosenElements);
-        for(String s: temp){
-            System.out.println(s);
+        for (String s : temp) {
+            vbox = addBooking(vbox, s);
         }
+    }
+
+    private VBox addBooking(VBox vbox, String str) {
+        vbox.getChildren().add(new Button(str));
+        return vbox;
     }
 
     public static void main(String[] args) {
@@ -129,19 +140,24 @@ class Section {
 
     public Section(String title) {
         titleLabel.setText(title);
-        titleLabel.setPrefHeight(70);
-        titleLabel.setPrefWidth(140);
+        titleLabel.setMinHeight(30);
+        titleLabel.setMinWidth(50);
         dateLabel.setText("Date");
+        dateLabel.setMinWidth(30);
         localLabel.setText("Location");
+        localLabel.setMinWidth(30);
         for (Integer i : db.startDateArr) {
             dateComboBox.getItems().add(db.parseDate(i));
         }
         for (String s : db.localArr) {
             localComboBox.getItems().add(s);
         }
+        dateComboBox.setMaxWidth(100);
+        localComboBox.setMaxWidth(135);
         dateVBox = new VBox(dateLabel, dateComboBox);
         localVBox = new VBox(localLabel, localComboBox);
         contentHBox = new HBox(dateVBox, localVBox);
         fullVBox = new VBox(titleLabel, contentHBox);
+        fullVBox.setPadding(new Insets(20,0,0,0));
     }
 }
